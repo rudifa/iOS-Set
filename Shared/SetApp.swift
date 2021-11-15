d4bc8ce28daf4bc3c0e06b598e3bfeee38e2639b
Rudi Farkas <rudi.farkas@gmail.com>
2021-11-15 15:45:21 +0100

    Initial Commit

---
+++ Shared/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Shared
+//
+//  Created by Rudolf Farkas on 15.11.21.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
