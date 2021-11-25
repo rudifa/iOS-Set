639b90f5153fa032e327e62996e780e6685b647a
Rudi Farkas <rudi.farkas@gmail.com>
2021-11-25 20:35:26 +0100

    move windowSize to SetApp

--- Shared/App/SetApp.swift
+++ Shared/App/SetApp.swift
@@ -7,11 +7,21 @@
 
 import SwiftUI
 
+#if os(macOS)
+    let windowSize = CGSize(width: 450, height: 800)
+#endif
+
 @main
 struct SetApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+            #if os(macOS)
+                .frame(minWidth: windowSize.width,
+                       maxWidth: windowSize.width,
+                       minHeight: windowSize.height,
+                       maxHeight: windowSize.height)
+            #endif
         }
     }
 }
